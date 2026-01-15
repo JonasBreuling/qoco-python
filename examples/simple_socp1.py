71e87bd023367c9d9583f4a6a7a41a185734441b
Jonas Breuling <jonas-breuling@inm.uni-stuttgart.de>
2026-01-15 07:55:38 +0100

    Add example that uses factorized kkt system.

---
+++ examples/simple_socp1.py
@@ -0,0 +1,42 @@
+import qoco
+import numpy as np
+from scipy import sparse
+
+
+if __name__ == "__main__":
+    # Define problem data
+    P = sparse.diags([1, 2, 3, 4, 5, 6], 0, dtype=None)
+    P = P.tocsc()
+
+    c = np.array([1, 2, 3, 4, 5, 6])
+    G = -sparse.identity(6)
+    G = G.tocsc()
+    h = np.zeros(6)
+    A = sparse.csc_matrix([[1, 1, 0, 0, 0, 0], [0, 1, 2, 0, 0, 0]])
+    A = A.tocsc()
+    b = np.array([1, 2])
+
+    l = 3
+    n = 6
+    m = 6
+    p = 2
+    nsoc = 1
+    q = np.array([3])
+
+    # Create an QOCO object.
+    prob = qoco.QOCO()
+
+    # Setup workspace.
+    prob.setup(n, m, p, P, c, A, b, G, h, l, nsoc, q)
+
+    # Solve problem.
+    res = prob.solve()
+
+    opt_obj = 4.042
+    assert res.status == "QOCO_SOLVED"
+    assert abs(res.obj - opt_obj) <= 1e-4
+
+    rhs = np.zeros(prob.n + prob.m + prob.p)
+    sol = prob.solve_kkt(rhs)
+
+    print("Solution:", sol)
